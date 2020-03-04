33348a652a7a228d07f490321576972f8cdcdc06
황선아 <tjsdk2769@gmail.com>
2020-03-04 18:53:25 +0900

    remove setter from repositories

--- src/main/java/com/example/openapi/dto/MovieDTO.java
+++ src/main/java/com/example/openapi/dto/MovieDTO.java
@@ -7,13 +7,12 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter
-//@Setter
-//setter는 없어도 되지않을까? api 결과로 우선 movie로 가져오고 그 이후 movieDTO로 매핑이기때문에..? 헷갈린다 
+@Setter
 public class MovieDTO {
     private List<Movie.Item> items;
 
     @Getter
-    //@Setter
+    @Setter
     public static class Item{
         private String title;
         private String link;
